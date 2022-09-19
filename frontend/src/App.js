dfdb453ce6e4db69d5b60d3a4f6f4a23a6cd9175
kamsansari007 <92922613+kamsansari007@users.noreply.github.com>
2022-09-19 16:39:07 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
